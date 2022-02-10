2f47e54103207406784ff42967d2f56c290ff7a9
sernscripts <sernscripts@gmail.com>
2022-02-10 14:27:17 +0000

    Forgot password and reset password functionality

--- accounts/views.py
+++ accounts/views.py
@@ -66,7 +66,8 @@ def login(request):
 
     if user is not None:
       auth.login(request, user)
-      return redirect('home')
+      messages.success(request, 'You are now logged in.')
+      return redirect('dashboard')
     else:
       messages.error(request, 'Invalid login credentials')
       return redirect('login')
@@ -98,15 +99,78 @@ def activate(request, uidb64, token):
     return redirect('register')
 
   
+@login_required(login_url = 'login')  
+def dashboard(request):
+  return render(request, 'accounts/dashboard.html') 
+
+
+def forgotPassword(request):
+  if request.method == 'POST':
+    email = request.POST['email']
+    if Account.objects.filter(email=email).exists():
+      user = Account.objects.get(email__iexact=email)
+
+      ### reset password email
+      current_site = get_current_site(request)
+      mail_subject = 'Reset your password'
+      message = render_to_string('accounts/reset_password_email.html', {
+        'user': user,
+        'domain': current_site,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': default_token_generator.make_token(user),
+      })
+
+      to_email = email
+      send_email = EmailMessage(mail_subject, message, to=[to_email])
+      send_email.send()
+
+      messages.success(request, 'Password reset email has been sent to your email address.')
+      return redirect('login')
+
+    else:
+      messages.error(request, 'Account does not exist!')
+      return redirect('forgotPassword')
+  return render(request, 'accounts/forgotPassword.html')
+
+
+def resetpassword_validate(request, uidb64, token):
+  try:
+    ### this will give us the primary key of the user
+    uid = urlsafe_base64_decode(uidb64).decode()
+    user = Account._default_manager.get(pk=uid)
+
+  except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+    user = None
+
+  if user is not None and default_token_generator.check_token(user, token):
+    request.session['uid'] = uid
+    messages.success(request, 'Please reset your password')
+    return redirect('resetPassword')
+  
+  else:
+    messages.error(request, 'This link has expired')
+    return redirect('login')
 
 
 
+def resetPassword(request):
+  if request.method == 'POST':
+    password = request.POST['password']
+    confirm_password = request.POST['confirm_password']
 
+    if password == confirm_password:
+      uid = request.session.get('uid')
+      user = Account.objects.get(pk=uid)
+      user.set_password(password)
+      user.save()
+      messages.success(request, 'Password reset successful.')
+      return redirect('login')
+    else:
+      messages.error(request, 'Passwords do not match')
+      return redirect('resetPassword')
 
-
-
-
-
+  else:
+    return render(request, 'accounts/resetPassword.html')
 
 
 
@@ -142,3 +206,9 @@ def activate(request, uidb64, token):
 ### http://localhost:8000/accounts/login/?command=verification&email=mkdbaah@gmail.com
 ### if there is a command of verification, then we are going to show the signin box again for the person(user) to type anything inside again
 ### if the command is not there then we show the signin page
+
+### email = request.POST['email']  , the email in the quotes that we are accessing is from the name in the html file
+
+### supposed to be Account.objects.get(email=email) but it is rather 'email__iexact=email' so that it will be exact and the i will make it case insentitive
+
+### in the resetPassword function, we don't need to pass the uidb64 becasuse it is already stored in the session
