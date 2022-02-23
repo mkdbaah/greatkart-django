86c203ee0d7541278a34e8d9d85fe2422ccbc576
sernscripts <sernscripts@gmail.com>
2022-02-23 04:25:46 +0000

    trying to collect the statics very well

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -148,12 +148,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR /'static'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR /'static'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     'greatkart/static',
 ]
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 ## media files configuration
 MEDIA_URL = '/media/'
