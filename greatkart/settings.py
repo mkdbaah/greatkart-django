20ae2ebdaf6f5fa84e2fa018404d3c615668aa6b
sernscripts <sernscripts@gmail.com>
2022-02-23 12:00:44 +0000

    trusted

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['sernprojectsgreatkart.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['sernprojectsgreatkart.herokuapp.com']
 
 
 # Application definition
